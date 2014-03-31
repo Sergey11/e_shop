6d8d52370d756a585bac82cdfb6ef845fb1b31ea
sergey11 <9wik@ukr.net>
2014-03-31 19:05:01 +0400

    CSS work finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 EShop::Application.routes.draw do
 
   get "static_pages/start"
-  get "static_pages/help"
+  get "static_pages/products"
   get "static_pages/about"
+  get "static_pages/faq"
   get "static_pages/contact"
 
   # The priority is based upon order of creation: first created -> highest priority.
